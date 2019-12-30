0b2f68cda706649c021c5a3ab5937b3fd050cb63
akenamon <49825984+akenamon@users.noreply.github.com>
2019-12-30 17:11:39 -0300

    added dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+## First we must install some dependencies
+apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://get.docker.com -o get-docker.sh | sh get-docker.sh
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+chmod +x /usr/local/bin/docker-compose && \
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+## Time to use docker compose to start all containers
+docker-compose up -d 
+
+
+## use docker to get rsyslog data 
 echo '
 #!/bin/bash
  check_security_state()
@@ -17,8 +37,13 @@ check_security_state
 ' > /usr/local/bin/ssh-trapper-check.sh && \
 chmod +x /usr/local/bin/ssh-trapper-check.sh
 
-
+## We are going to monitor login activity with  cron 
 echo 'SHELL=/bin/bash
 PATH=/usr/local/bin:/usr/bin:/bin
 */10 * * * * root /bin/bash -c /usr/local/bin/ssh-trapper-check.sh
 #' > /etc/cron.d/ssh-trapper-check
+
+clear
+
+echo "ssh trap was actived!"
+
